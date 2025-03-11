99b0653a8cd7fb65f29c54dccee9ea642d99cb51
Anand <anandkulkarni782@gmail.com>
2025-03-11 22:22:26 +0530

    Added Abstract Class Functionality

---
+++ src/main/java/org/example/AbstractClassPOC.java
@@ -0,0 +1,12 @@
+package org.example;
+public abstract class AbstractClassPOC {
+
+    abstract void add(); //Abstract Method  with void return type
+
+    abstract int sub(); // Abstract Method With Int Return Type
+
+    public void display() {
+        System.out.println("This is concrete method");
+    }
+}
+
---
+++ src/main/java/org/example/normalClass.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class normalClass extends AbstractClassPOC {
+
+    int a=10, b=5;
+    int result;
+    @Override
+    void add() {
+
+
+        result = a+b;
+        System.out.println("This is add method"+" "+" "+"Result is"+" "+result);
+    }
+
+    @Override
+    int sub() {
+        result = a-b;
+
+      System.out.println(result);
+        return result;
+
+    }
+
+    public static void main(String args[])
+    {
+        normalClass nc = new normalClass();
+        nc.add();
+        nc.sub();
+        nc.display();
+    }
+}
