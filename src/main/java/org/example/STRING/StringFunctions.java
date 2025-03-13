166b64f9bcd7e36cf22fb4620e9f8481ec330289
Anand <anandkulkarni782@gmail.com>
2025-03-13 23:16:21 +0530

    added content for String Topic

---
+++ src/main/java/org/example/STRING/StringFunctions.java
@@ -0,0 +1,23 @@
+package org.example.STRING;
+
+public class StringFunctions {
+
+    public  static void main(String [] args)
+    {
+         String s1 = "Anand";
+         String s2 = "Kulkarni";
+         System.out.println("Message From length()"+" "+s1.length()); // Length function will return the Length of String
+
+         String Result=s1.concat(s2); // concat Function will join two Different Strings and will create one string.
+         System.out.println("Message From concat()"+" "+Result);
+
+         System.out.println("Message From charAt() Function"+" "+ s1.charAt(2)); // charAt() Function will return character from specified index.
+
+
+
+
+
+    }
+
+
+}
