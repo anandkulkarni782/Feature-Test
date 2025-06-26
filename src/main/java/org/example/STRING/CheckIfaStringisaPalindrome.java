197ffc3eee9124ac3c6bbd2635106970a3944f62
Kulkarni <2097646@cognizant.com>
2025-06-26 10:49:43 +0530

    added more content and modifed some logic

--- src/main/java/org/example/STRING/CheckIfaStringisaPalindrome.java
+++ src/main/java/org/example/STRING/CheckIfaStringisaPalindrome.java
@@ -1,12 +1,8 @@
 package org.example.STRING;
 
 public class CheckIfaStringisaPalindrome {
-    public static boolean ispalandrom(String str){
-        return str.equals(new StringBuilder(str).reverse().toString());
-    }
-    public static void main(String [] args)
-    {
-       System.out.println(CheckIfaStringisaPalindrome.ispalandrom("PPAPP"));
-    }
-
+   public static void main(String [] args){
+       String Input = "ppAApp";
+       System.out.println(Input.equals(new StringBuffer(Input).reverse().toString()));
+   }
 }
